2cef197d7a8a86984e94f00b2c42cf281cee326a
cargnieli <cargnieli@yahoo.com>
2021-06-08 09:59:10 -0300

    Initial commit

---
+++ src/com/company/CharDemo2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CharDemo2 {
+
+    public static void main(String[] args) {
+	char ch1;
+	ch1 = 'X';
+        System.out.println("ch1 contém " + ch1);
+
+        ch1++;
+        System.out.println("ch1 agora é: " + ch1);
+    }
+}
